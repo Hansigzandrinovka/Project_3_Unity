fd8e47baf5df558a60f344ff2038d75a28bc7682
DESKTOP-PCV9COG\Hans <hansskywalkerr@gmail.com>
2016-10-22 11:42:05 -0500

    Finished class definition for TileMonoBehavior.       Has prototype (unfinished) StepInDirection method,      working IsWalkable() method that can be altered (IE flying Entities),      working IsOccupied method (is there already an Entity here.      Works with Entity code to move Entities.
    Edited mainroom to have entities with Entity script and TestPlayerController attached.
    
    Prototype class definition for Entity
          Made Entities moveable with Move() method, taking an Entity-defined Enum direction.
          Entities can TakeDamage (untested), can goToTile, where essentially Move checks if Tile not occupied and walkable, then goes to tile.
    
    Added TestPlayerController
         finds an attached Entity and controls it
         reads "Horizontal" and "Vertical" inputs - Go to Edit->Project Settings->Input to define player Inputs, Horizontal and Vertical are float magnitude values watching WASD and arrow keys
        uses coded clock to regulate input frequency,
        calls Entity's Move method when Player wants to move Entitty
    
    Added missing documentation created yesterday:
         gant_list - to be part of the creation of Gantt Chart
         notesForProject3alt - the partially revised plan for Project 3 Class Diagram
        pros_cons_Dungeon_vs_Tiles - a debate session concerning a Tile array vs Tile-to-Tile pointing - led to implementation of Tile-to-Tile pointing system we have now
        tilingimplementation - documents the system I figured out for tile bridging (to some extent)
        added/added to CitedSources document that tracks all sources I referenced online and how I used them.
    
    Added to .gitignore
        .csproj - should clear up issues with tracking changes made whenever we open Visual Studio or Unity Editor.
        .dwlt
        .mdb
        InspectorExpandedItems.asset
        assetDatabase3
        .sln
        .userprefs - keep an eye on this file if things start going wrong, possible userprefs is the candidate.
    
    Removed:
        Project_3_Class_Details_WIP - deprecated due to existence of notesForProject3alt
        Assembly-CSharp-vs.csproj - appears to change with any saving or opening of Unity or Visual Studio, so no reason to track it.
       Assembly-CSharp.csproj ^^^^^^^^^^^
        CurrentLayout.dwlt
        ... and a bunch of other files detailed in commit log.

--- Wizard Needs Matches/Assets/Scripts/Entity.cs
+++ Wizard Needs Matches/Assets/Scripts/Entity.cs
@@ -4,15 +4,92 @@ using System.Collections;
 public class Entity : MonoBehaviour { //testtest
     public int health = 10;
     public int speed = 1; //number of actions entity can perform on a given turn
-    protected int remainingSpeed = 0; //number of remaining actions entity can take
+    protected int remainingSpeed = 1; //number of remaining actions entity can take
+    public int delay = 0; //number of turns entity must forfeit after an action before resuming action
+    protected int remainingDelay; //number of remaining turns before next action
     public TileMonoBehavior occupyingTile; //the tile this entity stands on, and its entry point into moving around on the board
 
+    public enum MoveDirection { up, down, left, right }; //the direction player wishes to move for the purpose of Move function
+    public enum DamageType { poking }; //the types of damage that an entity can take for the purpose of the TakeDamage function
+
+    //tells the DungeonController this entity is done acting
+    public void EndTurn()
+    { }
+
+    //by default, generic entities do not know any spells
+    public virtual bool CastSpell(int spellIndex)
+    {
+        return false;
+    }
+
+    public bool TakeDamage(int amount, DamageType type)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            //TODO: make entity explode into money, candy, fireworks, etc.
+            return true;
+        }
+        return false;
+            
+    }
+
+    //tries to move this entity in direction, returns false on fail
+    //fail can be because out of movement, or because tile is occupied, or because not this entity's turn
+    public virtual bool Move(MoveDirection givenDirection)
+    {
+        if (remainingSpeed == 0 || remainingDelay > 0)
+        {
+            Debug.Log("Entity speed is depleted");
+            return false;
+        }
+            
+        TileMonoBehavior targetTile;
+        switch(givenDirection) //finds tile in given direction, tries to move there
+        {
+            case (MoveDirection.up):
+                targetTile = occupyingTile.getAbove();
+                break;
+            case (MoveDirection.left):
+                targetTile = occupyingTile.getLeft();
+                break;
+            case (MoveDirection.down):
+                targetTile = occupyingTile.getBelow();
+                break;
+            default:
+                targetTile = occupyingTile.getRight();
+                break;
+        }
+        if (targetTile == null)
+        {
+            //Debug.Log("No Tile Found to move to");
+            return false;
+        }
+            
+        //if is occupied, return false, if is not walkable return false
+        //otherwise return true
+        if (!targetTile.IsOccupied() & targetTile.IsWalkable())
+        {
+            goToTile(targetTile);
+            return true;
+        }
+        else
+        {
+            //Debug.Log("Was occupied or not walkable");
+            return false;
+        }
+            
+    }
+
     //moves this Enity to the specified Tile and begins tracking it
+    //assumes it is already possible for Entity to move to designated Tile (tile is walkable, not occupied
     public void goToTile(TileMonoBehavior newTile)
     {
+        if(occupyingTile != null)
+            occupyingTile.occupyingEntity = null; //makes old tile not think Entity is still there
         occupyingTile = newTile;
         occupyingTile.occupyingEntity = this;
-        transform.position.Set(occupyingTile.transform.position.x, occupyingTile.transform.position.y, occupyingTile.transform.position.z);
+        transform.position = new Vector3(occupyingTile.transform.position.x, occupyingTile.transform.position.y, transform.position.z);
     }
 
     // Update is called once per frame
