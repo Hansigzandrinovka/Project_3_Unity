d14ff3310d06d9461cd7c5fc1b31ac5061f6263d
DESKTOP-PCV9COG\Hans <hansskywalkerr@gmail.com>
2016-10-23 10:35:44 -0500

    Changed TestScene, added missing files from old project.

---
+++ Wizard Needs Matches/Assets/Scripts/DungeonManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DungeonManager : MonoBehaviour {
+
+    //public GameObject[] tileset; //holds Prefabs used to tile room
+    public static Queue turnOrder; //holds all controllers that can act
+    //when a controller finishes acting, it tells DungeonManager that its turn is finished, and DungeonManager grabs next actor in Queue and calls its StartTurn method
+
+    // Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
