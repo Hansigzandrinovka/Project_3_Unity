5c9aa675f34fa29fe927d847301a6ad3bc3136f7
Hansigzandrinovka <hansskywalkerr@gmail.com>
2016-10-30 10:12:41 -0600

    merged Gem Game assets into Wizard Needs Matches folder stopped tracking metadata because it appears to be created with each save Added Library/meta folders to gitignore list

---
+++ Wizard Needs Matches/Assets/Scripts/spellController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spellController : MonoBehaviour {
+	
+	public int damageCaused = 10;
+	public TileMonoBehavior occupyingTile; //the tile this entity stands on, and its entry point into moving around on the board
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 topRightLoc = new Vector2(transform.position.x + 0.25f, transform.position.y + 0.25f);
+		Vector2 botLeftLoc = new Vector2(transform.position.x - 0.25f, transform.position.y - 0.25f);
+		Collider2D collider = Physics2D.OverlapArea(topRightLoc, botLeftLoc, TileMonoBehavior.tileLayerMask);
+		if(collider != null)
+		{
+			TileMonoBehavior tileBeneathUs = collider.gameObject.GetComponent<TileMonoBehavior>();
+			if(tileBeneathUs != null)
+			{
+				if(!tileBeneathUs.IsWalkable())
+				{
+					Destroy(this.gameObject);
+				}
+				else if(tileBeneathUs.IsOccupied())
+				{
+					tileBeneathUs.occupyingEntity.TakeDamage(damageCaused,Entity.DamageType.poking);
+					Destroy(this.gameObject):
+				}
+				else
+				{
+					this.occupyingTile = tileBeneathUs;
+				}
+			}
+			else
+			{
+				Destroy(this.gameObject);
+			}
+		}
+	}
+}
