d2beccd169d44a275ba53778216c650a50c799a6
DESKTOP-PCV9COG\Hans <hansskywalkerr@gmail.com>
2016-11-03 16:03:09 -0500

    10x10 tile prefabs are born when the test level starts. Minor cleanup to Tile script

---
+++ Wizard Needs Matches/Assets/Resources/Scripts/DungeonGenerator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DungeonGenerator : MonoBehaviour {
+    //uses a RNG to create dungeon tiles through rooms connected by hallways
+
+    public static int testingSeed = 5; //the seed for the RNG system to predictably produce random tiles
+    public GameObject floorTilePrefab; //the tile used to generate the floor of a given dungeon
+
+	// Use this for initialization
+	void Start () {
+        Random.seed = testingSeed;
+        for(int i = 0; i < 10; i++)
+            for(int j = 0; j < 12; j++)
+            {
+                Instantiate(floorTilePrefab, new Vector3(i, j), floorTilePrefab.transform.rotation);
+            }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
