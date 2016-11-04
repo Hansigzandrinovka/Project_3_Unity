2dd6e989839efb2e84b28e7b827fb9c9a1f9dfc2
fisherc516 <c086f573@ku.edu>
2016-11-04 18:13:22 -0500

    Spells have types and change the tile material
    Fire tiles will burn you if you step on them
    To do: tiles change back to default after so many turns

--- Wizard Needs Matches/Assets/Resources/Scripts/spellController.cs
+++ Wizard Needs Matches/Assets/Resources/Scripts/spellController.cs
@@ -4,8 +4,36 @@ using System.Collections;
 public class spellController : MonoBehaviour {
 	
 	public int damageCaused = 10;
+	public enum spellType
+	{
+		regular,
+		fire,
+		ice,
+		lightning
+	};
+	public spellType type = spellType.regular;
+	public Material defaultMaterial;
 	public TileMonoBehavior occupyingTile; //the tile this entity stands on, and its entry point into moving around on the board
 	
+	void Start()
+	{
+		defaultMaterial = GetComponent<SpriteRenderer>().material;
+		if(type == spellType.regular)
+		{
+			GetComponent<SpriteRenderer>().color = Color.white;
+		}
+		else if(type == spellType.fire)
+		{
+			GetComponent<SpriteRenderer>().color = Color.red;
+		}
+		else if(type == spellType.ice)
+		{
+			GetComponent<SpriteRenderer>().color = Color.blue;
+		}
+		else
+		{
+			GetComponent<SpriteRenderer>().color = Color.yellow;
+		}
 	// Update is called once per frame
 	void Update () {
 		Vector2 topRightLoc = new Vector2(transform.position.x + 0.25f, transform.position.y + 0.25f);
@@ -29,6 +57,23 @@ public class spellController : MonoBehaviour {
 				else
 				{
 					this.occupyingTile = tileBeneathUs;
+					if(type == spellType.regular)
+					{
+						tileBeneathUs.GetComponent<SpriteRenderer>().material = defaultMaterial;
+					}
+					else if(type == spellType.fire)
+					{
+						tileBeneathUs.GetComponent<SpriteRenderer>().material = Resources.Load("Materials/Red") as Material;
+					}
+					else if(type == spellType.ice)
+					{
+						tileBeneathUs.GetComponent<SpriteRenderer>().material = Resources.Load("Materials/Blue") as Material;
+					}
+					else
+					{
+						tileBeneathUs.GetComponent<SpriteRenderer>().material = Resources.Load("Materials/Yellow") as Material;
+					}
+					//TODO: material changes back to default after a few turns
 				}
 			}
 			else
