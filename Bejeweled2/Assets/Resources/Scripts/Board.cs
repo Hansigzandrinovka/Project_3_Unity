ffdb7f0f9d0f3345a348e758b5fbf6411b24ce60
unknown <hfbrown@1005c-15.eecs.ku.edu>
2016-11-18 10:27:52 -0600

    //TODO: finish this from video #19

--- Bejeweled2/Assets/Resources/Scripts/Board.cs
+++ Bejeweled2/Assets/Resources/Scripts/Board.cs
@@ -6,38 +6,39 @@ using System.Collections.Generic;
 public class Board : MonoBehaviour {
 
 
-public List<Gem> gems = new List<Gem>();
-public int GridWidth;
-public int GridHeight;
-public GameObject gemPrefab;
-public Gem lastGem;
-public Vector3 gem1Start, gem1End, gem2Start, gem2End;
-public bool isSwapping = false;
-public Gem gem1,gem2;
-public float startTime;
-public float swapRate = 2 ;
-public int AmountToMatch = 3;
-public bool isMatched = false;
+    public List<Gem> gems = new List<Gem>();
+    public int GridWidth;
+    public int GridHeight;
+    public GameObject gemPrefab;
+    public Gem lastGem;
+    public Vector3 gem1Start, gem1End, gem2Start, gem2End;
+    public bool isSwapping = false;
+    public Gem gem1,gem2;
+    public float startTime;
+    public float swapRate = 2 ;
+    public int AmountToMatch = 3;
+    public bool isMatched = false;
 
 	
-// Use this for initialization
+    // Use this for initialization
 	
-void Start () {
+    //on start, create all the gems for user to match with, then move this object to a convenient location
+    void Start () {
 
-for(int y=0; y<GridHeight ; y++){
-for(int x=0; x<GridWidth; x++){
-GameObject g = Instantiate(gemPrefab,new Vector3(x,y,0),Quaternion.identity)as GameObject;
-g.transform.parent = gameObject.transform;
-gems.Add(g.GetComponent<Gem>());
-}
-}
-gameObject.transform.position = new Vector3(- 3.5f, -3.5f,0);
+        for(int y=0; y<GridHeight ; y++){
+            for(int x=0; x<GridWidth; x++){
+                GameObject g = Instantiate(gemPrefab,new Vector3(x,y,0),Quaternion.identity)as GameObject;
+                g.transform.parent = gameObject.transform;
+                gems.Add(g.GetComponent<Gem>());
+            }
+        }
+        //positions board at approximately center
+        gameObject.transform.position = new Vector3(- 3.5f, -3.5f,0);
+    }
+
 	
-	}
-
-	
-	// Update is called once per frame
-
+	    // Update is called once per frame
+    //every update, either match current gems, swap gems, or moniter board state
 	void Update () {
 
 
@@ -70,27 +71,53 @@ gameObject.transform.position = new Vector3(- 3.5f, -3.5f,0);
 				CheckMatch ();
 
 			}
-		} else if( DetermineBoardState() ){
+		} else if(!DetermineBoardState() ){ //if board is stable unmoving, check for additional matches
 			for (int i=0; i<gems.Count; i++){
 				CheckForNearbyMatches( gems[i] );
 
 			}
 		}
+        //make board clear if no viable matches
 	
 	}
 
+    //checks if board contains matches by trying all gems for matching potential
+    public bool DoesBoardContainMatches()
+    {
+        TogglePhysics(false);
+        for(int i = 0; i < gems.Count; i++)
+        {
+            for(int j = 0; j < gems.Count; j++)
+            {
+                if(gems[i].IsNeighborWith(gems[j])) //try to swap neighboring gems, see if match
+                {
+                    Gem g = gems[i];
+                    Gem f = gems[j];
+                    Vector3 tempPos = g.transform.position;
+                    g.transform.position = f.transform.position;
+                    f.transform.position = tempPos;
+                    List<Gem> testNeighbors = new List<Gem>(g.Neighbors);
+                    //TODO: finish this from video #19
+                }
+            }
+        }
+    }
 
+    //checks if board is in a stable state (we can check for matches)
+    //returns true if board is NOT in a stable state, else false
 	public bool DetermineBoardState()
 	{
-		for (int i=0; i < gems.Count; i++) {
+        //TODO: relate "8" to cols + transform.position.y
+		for (int i=0; i < gems.Count; i++) { //if there are gems above upper bounds of board
 			if(gems[i].transform.localPosition.y > 8)
 				return true;
-			else if( gems[i].GetComponent<Rigidbody>().velocity.y > .1f)
+			else if( Mathf.Abs(gems[i].GetComponent<Rigidbody>().velocity.y) > .1f) //if there is a gem that is still moving, board is not stable
 				return true;
 		}
 		return false;
 	}
 
+    //attempts to find matches for Gem 1 and Gem 2 (called after a swap finishes)
 	public void CheckMatch(){
 		List<Gem> gem1List = new List<Gem>();
 		List<Gem> gem2List = new List<Gem>();
@@ -103,6 +130,7 @@ gameObject.transform.position = new Vector3(- 3.5f, -3.5f,0);
 
 		}
 
+    //checks if there are matches involving given gem
 	public void CheckForNearbyMatches( Gem g)
 	{
 		List<Gem> gemList = new List<Gem> ();
@@ -110,7 +138,7 @@ gameObject.transform.position = new Vector3(- 3.5f, -3.5f,0);
 		FixMatchList ( g , gemList);
 	}
 
-
+    //builds a list of gems that can match with given gem at given position
 	public void ConstructMatchList( string color, Gem gem, int XCoord, int YCoord, ref List<Gem> MatchList ){
 		
 		if( gem == null){
@@ -130,9 +158,10 @@ gameObject.transform.position = new Vector3(- 3.5f, -3.5f,0);
 							}
 						}
 					}
-}
+    }
 
-
+    //converts single list of matching gems to two lists: rows and columns,
+    //checks if there are enough gems in either list to constitute a match
 	public void FixMatchList(Gem gem , List<Gem> ListToFix){
 
 		List<Gem> rows = new List<Gem>();
@@ -165,7 +194,8 @@ gameObject.transform.position = new Vector3(- 3.5f, -3.5f,0);
 
 		}
 
-
+    //Linearly interpolates (moves) a gem from "fromPos" to "toPos",
+    //rotates gem in 3d around central axis point 0,0,.1
 	public void MoveGem(Gem gemToMove, Vector3 toPos, Vector3 fromPos){
 		Vector3 center = (fromPos + toPos)* 0.5f;
 		center -= new Vector3(0,0,0.1f);
@@ -176,6 +206,8 @@ gameObject.transform.position = new Vector3(- 3.5f, -3.5f,0);
 		gemToMove.transform.position += center;
 	}
 
+    //linearly interpolates (moves) gem from "fromPos" to "toPos",
+    //rotates gem in 3d around central axis point 0,0,-.1
 	public void MoveNegGem(Gem gemToMove, Vector3 toPos, Vector3 fromPos){
 		Vector3 center = (fromPos + toPos)* 0.5f;
 		center -= new Vector3(0,0,-0.1f);
@@ -184,14 +216,18 @@ gameObject.transform.position = new Vector3(- 3.5f, -3.5f,0);
 		float fracComplete = (Time.time - startTime)/swapRate;
 		gemToMove.transform.position = Vector3.Slerp(riseRelCenter , setRelCenter , fracComplete );
 		gemToMove.transform.position += center;
-}
+    }
 
+    //enables (true) or disables (false) physics system for all gems,
+    //used to prevent gems falling while swapping two gems
 	public void TogglePhysics( bool isOn){
 		for( int i=0 ; i < gems.Count ; i++){
 			gems[i].GetComponent<Rigidbody>().isKinematic = isOn;
 			}
 		}
 	
+    //handles the logic of selecting/deselecting gems for swapping,
+    //sets first gem if undefined, else unsets first gem or sets second gem
 	public void SwapGems(Gem currentGem ){
 		if( lastGem == null ){
 			lastGem  = currentGem;}
@@ -218,6 +254,6 @@ gameObject.transform.position = new Vector3(- 3.5f, -3.5f,0);
 				}
 			}
 
-}
+    }
 
 }
--- Bejeweled2/Assets/Resources/Scripts/Gem.cs
+++ Bejeweled2/Assets/Resources/Scripts/Gem.cs
@@ -15,13 +15,17 @@ public class Gem : MonoBehaviour {
 	public List<Gem> Neighbors = new List<Gem>();
 	public bool isSelected = false;
 	public bool isMatched = false;
+    private Rigidbody attachedRigidbody;
+    private static readonly float maxUpwardSpeed = 2f;
 
+    //returns the approximate X location of the Gem as an int
 	public int XCoord{
 		get{
 			return Mathf.RoundToInt(transform.localPosition.x);
 			}
 		}
 
+    //returns the approximate Y location of the Gem as an int
 	public int YCoord{
 		get{
 			return Mathf.RoundToInt(transform.localPosition.y);
@@ -31,10 +35,15 @@ public class Gem : MonoBehaviour {
 
 
 	// Use this for initialization
-
+    //gives this Gem a random color
 	void Start () {
 
 		CreateGem();
+        attachedRigidbody = GetComponent<Rigidbody>();
+        if(attachedRigidbody == null)
+        {
+            Debug.LogError("Alert! No Rigidbody attached to Gem at " + transform.position.x + "," + transform.position.y);
+        }
 	
 	}
 
@@ -42,33 +51,62 @@ public class Gem : MonoBehaviour {
 	// Update is called once per frame
 
 	void Update () {
-
-	
+        ReduceUpwardBouncyness();
 	}
 
+    //tracks if this gem is selected or not,
+    //turns on/off the Particle effects indicating this Gem is selected
 	public void ToggleSelector(){
 		isSelected  = !isSelected ;
 		selector.SetActive(isSelected);
 }
+
+    //prevents the gem from bouncing upward with a high velocity when gems swap
+    //or when hitting a gem from above
+    void ReduceUpwardBouncyness()
+    {
+        if(attachedRigidbody != null && attachedRigidbody.velocity.y > maxUpwardSpeed)
+        {
+            //Debug.Log("Enforcing max Y Velocity on Gem");
+            attachedRigidbody.velocity = new Vector3(attachedRigidbody.velocity.x,0f,attachedRigidbody.velocity.z);
+        }
+
+    }
+
+    //sets current gem's color and appearance to a new color/appearance using Unity's RNG
+    //if unable to load resource, Logs an Error and destroys Object
 	public void CreateGem(){
 		color = gemMats[Random.Range(0,gemMats.Length)];
 		Material m = Resources.Load("Materials/"+color)as Material;
+        if(m == null)
+        {
+            Debug.LogError("Unable to fetch material at Materials/" + color);
+            Destroy(this.gameObject);
+            return;
+        }
 		sphere.GetComponent<Renderer>().material = m;
 }
 
+    //returns true if given Gem is one of this Gem's neighbors
 	public bool IsNeighborWith(Gem g){
 		if( Neighbors.Contains(g)){
 			return true;
 		}
 		return false;
 }
+    //adds Gem to this gem's neighbors if isn't already in neighbors
 	public void AddNeighbor(Gem g){
 		if(!Neighbors.Contains(g))
 			Neighbors.Add(g);
 		}
+
+    //attempts to remove given Gem from this gem's neighbors
 	public void RemoveNeighbor(Gem g){
 		Neighbors.Remove(g);
 		}
+
+    //called by Unity if this object is clicked with Mouse during runtime
+    //toggles particle effects if in swapping state, else attempts to swap gems
 	void OnMouseDown(){
 		
 		if( !GameObject.Find("Board").GetComponent<Board>().isSwapping){
