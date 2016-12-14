c9bdabfe2b941ef9720d6e9d0691b2f50df8d725
RinseAndSauceMan <pdreger@ku.edu>
2016-12-14 03:41:11 -0600

    Background music is on through levels, but same song loops.

---
+++ Wizard Needs Matches/Assets/Resources/Scripts/musicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class musicPlayer : MonoBehaviour {
+
+	public AudioSource BGM;
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(gameObject);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void ChangeMusic(AudioClip music)
+	{
+		BGM.Stop();
+		BGM.clip = music;
+		BGM.Play ();
+	}
+}
